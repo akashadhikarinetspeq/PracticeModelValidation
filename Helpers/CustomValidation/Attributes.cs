e987fa4585a6eab81adee6c1abdac0084fe3a735
akashadhikarinetspeq <akash.adhikari@netspeq.com>
2026-04-14 14:47:18 +0530

    Completed custom validation server client seperation

--- Helpers/CustomValidation/Attributes.cs
+++ Helpers/CustomValidation/Attributes.cs
@@ -1,21 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
-using System.Globalization;
 
-namespace PracticeModelValidation.Helpers
+namespace PracticeModelValidation.Helpers.CustomValidation
 {
-    public class ClassicMovieAttribute(int year) : ValidationAttribute, IClientModelValidator
+    public class ClassicMovieAttribute(int year) : ValidationAttribute
     {
         public int Year { get; } = year;
-
-        public void AddValidation(ClientModelValidationContext context)
-        {
-            CustomValidationHelpers.MergeAttribute(context.Attributes, "data-val", "true");
-            CustomValidationHelpers.MergeAttribute(context.Attributes, "data-val-classicmovie", GetErrorMessage());
-
-            var year = Year.ToString(CultureInfo.InvariantCulture);
-            CustomValidationHelpers.MergeAttribute(context.Attributes, "data-val-classicmovie-year", year);
-        }
 
         public string GetErrorMessage() => ErrorMessage ?? $"Classic movies must have a release year no later than {Year}.";
 
